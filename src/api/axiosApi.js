6d2c336831719cebe428bd67340b6ab2146bc164
Saneera Silva <saneera@eight25media.com>
2022-08-11 21:07:51 +0530

    add auto logout

--- src/api/axiosApi.js
+++ src/api/axiosApi.js
@@ -3,16 +3,18 @@ import config from "../config";
 
 const axiosApi = axios.create({
   baseURL: `${config.api.BASE_URL}${config.api.API_PREFIX}`,
-  headers: JSON.parse(localStorage.getItem("userData")).token
-    ? {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${
-          JSON.parse(localStorage.getItem("userData")).token
-        }`,
-      }
-    : {
-        "Content-Type": "application/json",
-      },
+  headers:
+    JSON.parse(localStorage.getItem("userData")) &&
+    JSON.parse(localStorage.getItem("userData")).token
+      ? {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${
+            JSON.parse(localStorage.getItem("userData")).token
+          }`,
+        }
+      : {
+          "Content-Type": "application/json",
+        },
 });
 
 export default axiosApi;
